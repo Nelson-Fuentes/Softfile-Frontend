de027ef2adb7a2d34c7349e9f2c8717129a468fc
billhollow <40575441+billhollow@users.noreply.github.com>
2021-07-04 13:41:07 -0500

    Module Studies - frontend - v1

--- src/app/components/admin/dynamic/study/study.component.ts
+++ src/app/components/admin/dynamic/study/study.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { StudyCenter, StudyCenterUser } from 'src/app/models/study';
+import { StudyService } from 'src/app/services/study/study.service';
 @Component({
   selector: 'app-study',
   templateUrl: './study.component.html',
@@ -7,7 +10,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudyComponent implements OnInit {
 
-  constructor() { }
+  public study_centers: StudyCenter[] = [];
+  public study_center_user: StudyCenterUser [] = [];
+  public current_page:number = 1;
+  public add_study_center: StudyCenter = new StudyCenter('');
+  public add_study_center_form: FormGroup;
+
+  constructor(
+    private studyService : StudyService,
+    private formBuilder: FormBuilder,
+    private toastrService: ToastrService
+  ) { 
+    this.add_study_center_form = this.make_study_form_group();
+
+    // when services are fully implemented, here add subscribe function
+    this.study_centers = this.studyService.get_study_centers();
+    // when services are fully implemented, here add subscribe function
+    this.study_center_user = this.studyService.get_study_center_user();
+
+  }
+
+  public make_study_form_group(){
+    return this.formBuilder.group({
+      name: new FormControl('', Validators.required)
+    });
+  }
+
+  public study_center_name_field_is_valid(form: FormGroup):boolean{
+    const control = form.get('name');
+    if (control){
+      return control.invalid && control.touched;
+    }
+    return false;
+  }
+
+  public add_study_center_fun(){
+    // when services are fully implemented, here add subscribe function
+    this.studyService.add_study_center(this.add_study_center);
+    this.study_centers.push(this.add_study_center);
+    this.toastrService.success(undefined, 'Registro exitoso')
+    this.add_study_center = new StudyCenter('');
+    this.add_study_center_form.reset();
+
+  }
+
+  public delete_study_center(i: number){
+    const study_center: StudyCenter = this.study_centers[i];
+    if (confirm('¿Estas seguro de eliminar este estudio?')){
+      this.study_centers.splice(i,1);
+      this.studyService.delete_study_center(study_center);
+      this.toastrService.success(undefined, 'El estudio ha sido borrado')
+
+    }
+  }
+
+  public validate_study_center(study_center: StudyCenter){
+    const name_control: FormControl = new FormControl('', Validators.required);
+    name_control.setValue(study_center.name);
+    return name_control.valid;
+  }
+
+  public update_study_center(study_center: StudyCenter){
+    this.studyService.update_study_center(study_center);
+    this.toastrService.success(undefined, 'Estudio Actualizado');
+    study_center.name = study_center.name;
+
+  }
 
   ngOnInit(): void {
   }
--- src/app/services/study/study.service.ts
+++ src/app/services/study/study.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { StudyCenter, StudyCenterUser } from 'src/app/models/study';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudyService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  // this arr will be fill with test data until services is implemented
+  test_study_center: StudyCenter [] = [{name: 'study center1', _id: 'first_id'}, {name: 'study center2', _id: 'second_id'}]
+  test_study_center_user: StudyCenterUser [] = [{study_center: this.test_study_center[0]}, {study_center: this.test_study_center[1]}]
+  public get_study_centers(){
+    return this.test_study_center;
+  }
+
+  public get_study_center_user(){
+    return this.test_study_center_user;
+  }
+  
+  public add_study_center(study_center: StudyCenter){
+    this.test_study_center.push(study_center);
+    console.log("added!");
+  }
+
+  public delete_study_center(study_center: StudyCenter){
+    this.test_study_center = this.test_study_center.filter(item=>item._id != study_center._id);
+    console.log("deleted!");
+  }
+
+  // now it works as a GET method
+  public update_study_center(study_center: StudyCenter){
+    console.log("updated!");
+    return this.test_study_center;
+  }
+
 }
+
