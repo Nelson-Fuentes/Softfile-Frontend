859915fc7bc174192f6f6ce8a9fe216c6140f0ac
Nelson Alejandro Fuentes Paredes <nelsonafp12@gmail.com>
2021-07-29 19:13:07 -0500

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  softfile_api_url : 'https://softfile.herokuapp.com/api/v1/',
+  token_authentication_key : 'token_authentiation'
 };
